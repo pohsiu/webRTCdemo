a1a88c9a22cb6e6352f61306c377ad29d5eb1055
Louis <louis.hsiao@d8ai.com>
2018-11-23 16:52:34 +0800

    as func

---
+++ src/index.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  TextInput,
+} from 'react-native';
+import {
+  RTCView,
+} from 'react-native-webrtc';
+import io from 'socket.io-client';
+
+import {
+  pcPeers,
+  exchange,
+  leave,
+  getLocalStream,
+} from './rtc-helper';
+
+
+const socket = io.connect('https://react-native-webrtc.herokuapp.com', {transports: ['websocket']});
+
+let localStream;
+
+const mapHash = (hash, func) => {
+  const array = [];
+  for (const key in hash) {
+    const obj = hash[key];
+    array.push(func(obj, key));
+  }
+  return array;
+}
+
+class RCTWebRTCDemo extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      info: 'Initializing',
+      status: 'init',
+      roomID: '',
+      isFront: true,
+      selfViewSrc: null,
+      remoteList: {},
+      textRoomConnected: false,
+      textRoomData: [],
+      textRoomValue: '',
+    };
+  }
+  componentDidMount(){
+    socket.on('exchange', (data) => {
+      exchange(socket, data);
+    });
+    socket.on('leave', (socketId) => {
+      leave(socketId);
+    });
+    
+    socket.on('connect', (data) => {
+      console.log('connect');
+      getLocalStream(true, (stream) => {
+        localStream = stream;
+        this.setState({selfViewSrc: stream.toURL()});
+        this.setState({status: 'ready', info: 'Please enter or create room ID'});
+      });
+    });
+  }
+  
+  press = (event) => {
+    this.refs.roomID.blur();
+    this.setState({status: 'connect', info: 'Connecting'});
+    join(socket, this.state.roomID);
+  }
+  switchVideoType = () => {
+    const isFront = !this.state.isFront;
+    this.setState({isFront});
+    getLocalStream(isFront, (stream) => {
+      if (localStream) {
+        for (const id in pcPeers) {
+          const pc = pcPeers[id];
+          pc && pc.removeStream(localStream);
+        }
+        localStream.release();
+      }
+      localStream = stream;
+      this.setState({selfViewSrc: stream.toURL()});
+
+      for (const id in pcPeers) {
+        const pc = pcPeers[id];
+        pc && pc.addStream(localStream);
+      }
+    });
+  }
+  receiveTextData = (data) => {
+    const textRoomData = this.state.textRoomData.slice();
+    textRoomData.push(data);
+    this.setState({textRoomData, textRoomValue: ''});
+  }
+  textRoomPress = () => {
+    if (!this.state.textRoomValue) {
+      return
+    }
+    const textRoomData = this.state.textRoomData.slice();
+    textRoomData.push({user: 'Me', message: this.state.textRoomValue});
+    for (const key in pcPeers) {
+      const pc = pcPeers[key];
+      pc.textDataChannel.send(this.state.textRoomValue);
+    }
+    this.setState({textRoomData, textRoomValue: ''});
+  }
+  renderTextRoom() {
+    return (
+      <View style={styles.listViewContainer}>
+        {/* <ListView
+          dataSource={this.ds.cloneWithRows(this.state.textRoomData)}
+          renderRow={rowData => <Text>{`${rowData.user}: ${rowData.message}`}</Text>}
+          /> */}
+        <TextInput
+          style={{width: 200, height: 30, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={value => this.setState({textRoomValue: value})}
+          value={this.state.textRoomValue}
+        />
+        <TouchableHighlight
+          onPress={this.textRoomPress}>
+          <Text>Send</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {this.state.info}
+        </Text>
+        {this.state.textRoomConnected && this.renderTextRoom()}
+        <View style={{flexDirection: 'row'}}>
+          <Text>
+            {this.state.isFront ? "Use front camera" : "Use back camera"}
+          </Text>
+          <TouchableHighlight
+            style={{borderWidth: 1, borderColor: 'black'}}
+            onPress={this.switchVideoType}>
+            <Text>Switch camera</Text>
+          </TouchableHighlight>
+        </View>
+        { this.state.status == 'ready' ?
+          (<View>
+            <TextInput
+              ref='roomID'
+              autoCorrect={false}
+              style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({roomID: text})}
+              value={this.state.roomID}
+            />
+            <TouchableHighlight
+              onPress={this.press}>
+              <Text>Enter room</Text>
+            </TouchableHighlight>
+          </View>) : null
+        }
+        <RTCView streamURL={this.state.selfViewSrc} style={styles.selfView}/>
+        {
+          mapHash(this.state.remoteList, function(remote, index) {
+            return <RTCView key={index} streamURL={remote} style={styles.remoteView}/>
+          })
+        }
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  selfView: {
+    width: 200,
+    height: 150,
+  },
+  remoteView: {
+    width: 200,
+    height: 150,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  listViewContainer: {
+    height: 150,
+  },
+});
+
+export default RCTWebRTCDemo;
